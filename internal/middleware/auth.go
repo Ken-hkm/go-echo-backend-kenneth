7c4747669b412466b8e2f5fb0558c8d4856728c3
kenneth.hakim <kenneth.hakim@sg-edts.com>
2025-02-25 21:07:07 +0700

    implement amazon ssm parameters as secret storage, to keep the cost on zero

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,17 +1,21 @@
 package middleware
 
 import (
+	"github.com/Ken-hkm/go-echo-backend-kenneth/internal/secrets"
 	"github.com/labstack/echo/v4"
+	"log"
 	"net/http"
 )
 
 func SecretKeyAuth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		providedKey := c.Request().Header.Get("X-API-KEY")
-		//hardcode, to maintain zero cost (since we are using aws lambda on free tier)
-		//i did implement secrets feature but yeah, nope
-		expectedKey := "wRNbm38KGBO79fj"
-
+		expectedKey, err := secrets.GetParameter("go_api_key")
+		if err != nil {
+			log.Printf("Failed to get parameter: %v", err)
+			errMsg := "Failed to get parameter: " + err.Error()
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": errMsg})
+		}
 		if providedKey == "" || providedKey != expectedKey {
 			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
 		}
